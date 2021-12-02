391d8e08624e50a7f61e2d2ddc7f73bcde4ab772
Blanc Kévin <56817478+Zoinloz@users.noreply.github.com>
2021-12-02 20:57:53 +0100

    cors

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const express = require("express");
 const app = express();
 
 const db = require("./models");
-
 db.sequelize.sync();
 
+var cors = require('cors');
+app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -15,16 +16,15 @@ app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to LIVRE application." });
 });
-
-
 
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
+
   
   const genreRouter = require('./controllers/genre-controller');
   app.use('/', genreRouter);  
