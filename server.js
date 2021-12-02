697e13524fe6fbed7f5fe6baa343a928591c2fbf
Blanc Kévin <56817478+Zoinloz@users.noreply.github.com>
2021-12-02 20:55:36 +0100

    test

--- server.js
+++ server.js
@@ -7,13 +7,6 @@ const db = require("./models");
 db.sequelize.sync();
 
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  next();
-});
-
 // parse requests of content-type - application/json
 app.use(express.json());
 
